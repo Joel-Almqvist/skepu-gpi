5cbe19d0a86fed6698faf6f41d540e9cb8a91e13
Joel Almqvist <joeal360@student.liu.se>
2020-12-07 16:42:48 +0100

    Cleanup. Map is now functional for 2 arguments

--- src/skepu3/cluster/gpi/main.cpp
+++ src/skepu3/cluster/gpi/main.cpp
@@ -6,156 +6,14 @@
 #include <vector>
 
 
-template<typename T1, typename T2>
-T1 foofunc(T1 t1, T2 t2){
-  return t1 + t2;
-};
-
-void bugg_test(){
-  gaspi_proc_init(GASPI_BLOCK);
-  gaspi_rank_t rank;
-  gaspi_rank_t nr_nodes;
-
-  gaspi_proc_rank(&rank);
-  gaspi_proc_num(&nr_nodes);
-
-  gaspi_queue_id_t queue;
-  gaspi_queue_create(&queue, GASPI_BLOCK);
-
-  assert(gaspi_segment_create(
-    rank,
-    gaspi_size_t{200},
-    GASPI_GROUP_ALL,
-    GASPI_BLOCK,
-    GASPI_ALLOC_DEFAULT
-  ) == GASPI_SUCCESS);
-
-  gaspi_pointer_t seg_ptr;
-  gaspi_segment_ptr(rank, &seg_ptr);
-
-  if(rank == 0){
-    ((int*) seg_ptr)[0] = 2;
-    ((int*) seg_ptr)[1] = 3;
-  }
-
-  gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK);
-
-  if(rank == 0){
-
-    gaspi_notify(
-      0, // remote seg id
-      0, //rank
-      10, //notification id
-      33, //notif val
-      queue,
-      GASPI_BLOCK
-    );
-
-
-    gaspi_notification_id_t first_id;
-    gaspi_notify_waitsome(
-      0 , //seg id
-      10, // notification begin
-      2, // notification number
-      &first_id,
-      GASPI_BLOCK
-    );
-
-    gaspi_notification_t notify_val = 0;
-    gaspi_notify_reset(0, first_id, &notify_val);
-
-    std::cout << "Notify val: " << (int) notify_val << std::endl;
-
-    gaspi_notify_reset(0, first_id, &notify_val);
-
-    std::cout << "Notify val: " << (int) notify_val << std::endl;
-
-
-
-
-
-    // gaspi_write_notify(0, //loc seg id
-    //   0, //loc offset
-    //   0, //dest rank
-    //   0, //seg id remote
-    //   0, // offset remote
-    //   sizeof(int), // size
-    //   11, // notification id
-    //   333,
-    //   queue,
-    //   GASPI_BLOCK);
-
-
-    // gaspi_write_notify(0, //loc seg id
-    //   sizeof(int), //loc offset
-    //   1, //dest rank
-    //   1, //seg id remote
-    //   sizeof(int), // offset remote
-    //   sizeof(int), // size
-    //   10, // notification id
-    //   111,
-    //   queue,
-    //   GASPI_BLOCK);
-
-  }
-
-
-  else if(false && rank == 1){
-
-    gaspi_notification_id_t first_id;
-
-
-
-    gaspi_notify_waitsome(
-      0 , //seg id
-      11, // notification begin
-      2, // notification number
-      &first_id,
-      GASPI_BLOCK
-    );
-
-
-    gaspi_notification_t notify_val = 0;
-    gaspi_notify_reset(1, first_id, &notify_val);
-
-    std::cout << "Notify val: " << (int) notify_val << std::endl;
-
-
-    // gaspi_notify_waitsome(
-    //   1 , //seg id
-    //   10, // notification begin
-    //   2, // notification number
-    //   &first_id,
-    //   GASPI_BLOCK
-    // );
-
-    gaspi_notify_reset(1, first_id, &notify_val);
-
-    std::cout << "Notify val: " << (int) notify_val << std::endl;
-
-
-
-    std::cout << "First id: " << (int) first_id << std::endl;
-    std::cout << "Have vals: " << ((int*) seg_ptr)[0] <<", "
-    << ((int*) seg_ptr)[1] << std::endl;
-  }
-}
-
 
 int main(){
 
-  if(false){
-    bugg_test();
-    return 1;
-  }
-
-
 
   skepu::Matrix<long> m1{3,3,0};
-  skepu::Matrix<long> m2{4,4,2};
-  skepu::Matrix<long> m3{4,4,3};
+  skepu::Matrix<long> m2{10,10,2};
+  skepu::Matrix<long> m3{40,40,3};
 
-  //gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK);
 
   /*
   auto map = skepu::Map<2>([](long a, long b) int {
@@ -165,8 +23,8 @@ int main(){
 
   //skepu::Map<float, 2>(2.2);
 
-  auto map = skepu::Map<2>([](long a, long b) long {
-    return a*b;
+  auto map = skepu::Map<2>([](long a) long {
+    return a;
   });
 
   gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK);
--- src/skepu3/cluster/gpi/map.hpp
+++ src/skepu3/cluster/gpi/map.hpp
@@ -21,29 +21,38 @@ namespace skepu{
     Map1D(Function func) : func{func} {};
 
 
+    template<typename DestCont, typename Cont1, typename Cont2>
+     auto operator()(DestCont& dest_cont, Cont1& cont1, Cont2& cont2) ->
+     decltype(
+       std::declval<typename DestCont::is_skepu_container>(),
+       std::declval<typename Cont1::is_skepu_container>(),
+       std::declval<typename Cont2::is_skepu_container>(),
+
+       // Check that the lambda takes one argument
+       std::declval<Function>()(std::declval<typename DestCont::value_type>()),
+
+       std::declval<void>()) {
+         using T = typename DestCont::value_type;
+
+         std::cout << "One arg map not yet implemented\n";
+       }
+
+
      template<typename DestCont, typename Cont1, typename Cont2>
       auto operator()(DestCont& dest_cont, Cont1& cont1, Cont2& cont2) ->
       decltype(
         std::declval<typename DestCont::is_skepu_container>(),
         std::declval<typename Cont1::is_skepu_container>(),
         std::declval<typename Cont2::is_skepu_container>(),
+
+        // Check that the lambda takes two arguments
+        std::declval<Function>()(std::declval<typename DestCont::value_type>(),
+        std::declval<typename DestCont::value_type>()),
+
         std::declval<void>()) {
           using T = typename DestCont::value_type;
 
-
-          if(!std::is_same<T, long>::value){
-            std::cout << "NOT SAME\n";
-            return;
-          }
-
-
-
-          int nr_notifications = 1 + cont1.get_owner(dest_cont.end_i)
-              - cont1.get_owner(dest_cont.start_i);
-
-
           int smallest_seg_id = dest_cont.comm_segment_id - cont1.rank;
-
 
           // Each call to get range builds one nodes buffer
           for(int i = 0; i < cont1.nr_nodes; i++){
@@ -89,36 +98,17 @@ namespace skepu{
           }
 
 
-
           int nr_expected_notif_c1 = cont1.get_owner(dest_cont.end_i)
               - cont1.get_owner(dest_cont.start_i) + 1;
+
           int nr_expected_notif_c2 = cont2.get_owner(dest_cont.end_i)
               - cont2.get_owner(dest_cont.start_i) + 1;
 
-          int nr_notif = 0;
           gaspi_notification_t notify_val = 0;
           gaspi_notification_id_t first_id;
 
 
           for(int i = 0; i < nr_expected_notif_c1; i++){
-
-
-          //   if(dest_cont.rank != 3){
-          //     return;
-          //   }
-          //
-          //   std::cout << "Owner of " << dest_cont.start_i <<
-          //   " is: " << cont1.get_owner(dest_cont.start_i) << std::endl;
-          //
-          //
-          // // TODO get_range() skickar aldrig lokala delar till buffern
-          // std::cout << "Rank " << dest_cont.rank <<
-          // " seg " << (int) dest_cont.comm_segment_id  << " is waiting on seg "
-          // << (int) dest_cont.comm_segment_id << " for "
-          // << nr_expected_notif_c1 << " notifs starting at " <<
-          // cont1.get_owner(dest_cont.start_i) + 1 << std::endl;
-          // return;
-
             gaspi_notify_waitsome(
               dest_cont.comm_segment_id , //seg id
               cont1.get_owner(dest_cont.start_i) + 1, // notification begin
@@ -126,10 +116,9 @@ namespace skepu{
               &first_id,
               GASPI_BLOCK
             );
+
             gaspi_notify_reset(dest_cont.comm_segment_id, first_id, &notify_val);
 
-            std::cout << "Rank " << dest_cont.rank << " got notif: ("
-            << (int) first_id << ", " << (int) notify_val << ")\n";
           }
 
 
@@ -146,105 +135,18 @@ namespace skepu{
 
             gaspi_notify_reset(dest_cont.comm_segment_id, first_id, &notify_val);
 
-            std::cout << "Rank " << dest_cont.rank << " got notif: ("
-            << (int) first_id << ", " << (int) notify_val << ")\n";
           }
-
-
-          /*
-          std::cout << "Rank " << dest_cont.rank << " waiting for " <<
-          nr_notifications << " notifications, first id to wait for: "
-          << cont1.get_owner(dest_cont.start_i) <<
-          " has comm seg id: "<< (int) dest_cont.comm_segment_id <<std::endl;
-          */
 
           T* dest_cont_ptr = (T*) dest_cont.cont_seg_ptr;
           T* dest_comm_ptr = (T*) dest_cont.comm_seg_ptr;
           for(int i = 0; i < dest_cont.local_size; i++){
-            // std::cout << "Adding: " << dest_comm_ptr[i] << " and " <<
-            // dest_comm_ptr[i+dest_cont.local_size] << std::endl;
-            // std::cout << "loc size: " << dest_cont.local_size << std::endl;
-
             dest_cont_ptr[i] = func(dest_comm_ptr[i], dest_comm_ptr[i+dest_cont.local_size]);
           }
 
-          // TODO
-          // 1 - After we are done with this function we can NOT guarantee that
-          // that the remote state is correct. Hence we can NOT read nor write
-          // from remote containers.
-
-          // Send notifications to every node after every operation, then at the
-          // start of every function wait for the nodes which we will read from.
-
-          // Weak synchronization vs hard synchronization
-
-
-          /*
-          At the start of all function call hold(ranks)
-          which makes these rank sleep
-
-
-          A - At the end of all functions call notify to all nodes which are relying
-          on me.
-          B - Then wait for notifies from all nodes which rely on me.
-
-          OR replace B with a wait at the start of all functions and store the
-          relevant nodes in the matrix class. More error prone but possible better
-          performance
-
-          This scheme is hindered by the fact that all containers are distributed
-          equally among all nodes. Hence the difference from this and a barrier
-          is negligeble in the systems current state.
-
-          */
-
-
-
-          // 2 - Handle small containers
-
-          // 3 . Expand the interface to match SkePU, add iterators
-
-
-
-          // TODO remove this debug
-          for(int i = 0; i < 4; i++){
-            if(dest_cont.rank == i){
-              std::cout << "Rank " << i << ": ";
-              for(int j = 0; j < dest_cont.local_size; j++){
-                std::cout << dest_cont_ptr[j] << ", ";
-              }
-              std::cout << std::endl;
-            }
-            gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK);
-
-          }
-
-
-          // TODO change this
-          /*
+          // TODO replace with vector clock
           gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK);
-          nr_notifications = cont2.get_range(
-            dest_cont.start_i,
-            dest_cont.end_i,
-            dest_cont.rank, // This will be the same regardless of container
-            dest_cont.comm_segment_id,
-            (dest_cont.end_i + 1) * sizeof(cont1.value_type)
-          )
-          */
 
-          /*
-          if(is_skepu_container<Container>::value){
-            using T = typename Container::value_type;
-
-            // Prevents race conditions on the underlying container
-            gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK);
-
-            for(int i = 0; i < cont.local_size; i++){
-              ((T*) cont.cont_seg_ptr)[i] = func(((T*) cont.cont_seg_ptr)[i]);
-            }
-          }
-          */
-     } // end of () overload
+     } // end of () operator
 
 
 
